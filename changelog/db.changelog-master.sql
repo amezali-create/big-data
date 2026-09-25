920efdc2a3834347016a06f57c8b1323f57197a9
Amezali <amezali@ucvvirtual.edu.pe>
2026-09-25 17:17:19 -0500

    fix: include physical dimensional model in master changelog

--- changelog/db.changelog-master.sql
+++ changelog/db.changelog-master.sql
@@ -1,4 +1,8 @@
 --liquibase formatted sql
 
---changeset tu_nombre:1-create-bi-schema
-CREATE SCHEMA IF NOT EXISTS lab_bi_schema;
+--include file:changelog/001_create_bi_schema.sql
+--include file:changelog/003_create_dim_date.sql
+--include file:changelog/004_create_dim_product.sql
+--include file:changelog/005_create_dim_customer.sql
+--include file:changelog/006_create_dim_store.sql
+--include file:changelog/007_create_fact_sales.sql
