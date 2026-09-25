c13955d662b0cc013997381ff4201dfbb36d7618
Amezali <amezali@ucvvirtual.edu.pe>
2026-09-25 18:00:57 -0500

    fix: agregar contenido y encabezado liquibase a 003_create_dim_date.sql

---
+++ changelog/003_create_dim_date.sql
@@ -0,0 +1,16 @@
+﻿--liquibase formatted sql
+
+--changeset amezali:003_create_dim_date
+CREATE TABLE IF NOT EXISTS dim_date (
+    date_key INT,
+    full_date DATE,
+    day_of_week INT,
+    day_name STRING,
+    day_of_month INT,
+    day_of_year INT,
+    week_of_year INT,
+    month_num INT,
+    month_name STRING,
+    quarter INT,
+    year INT
+) USING DELTA;
